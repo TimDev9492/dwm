2146623f45414143528dec9f504ef92c575e3318
tim <tim@timdev.de>
2021-08-28 22:09:57 +0200

    applied namedscratchpads patch

--- config.h
+++ config.h
@@ -47,18 +47,19 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      		instance    	title			tags mask	isfloating	monitor */
-	{ "Gimp",     		NULL,   	NULL,       		0,     	       	1,           	-1 },
-	{ "firefox",  		NULL,   	NULL,       		1 << 4,       	0,           	-1 },
-	{ "discord",  		NULL,   	NULL,       		1 << 2,       	0,           	2 },
-	{ "lunarclient",	NULL,   	NULL,   		1 << 3,    	0,           	0 },
-	{ "MultiMC5",  		NULL,   	NULL,       		1 << 3,      	0,           	0 },
-	{ "Steam",  		NULL,		NULL,       		1 << 3,         0,           	-1 },
-	{ "KeePassXC",  	NULL,   	NULL,       		1 << 6,     	0,           	-1 },
-	{ "Signal",  		NULL,  	 	NULL,       		1 << 7,        	0,           	1 },
-	{ "FreeTube",  		NULL,  	 	NULL,       		1 << 9,        	0,           	1 },
-	{ "Yad",  		    NULL,  	 	NULL,       		0,        	    1,           	0 },
-	{ NULL,	      		NULL,		"Picture in picture", 	0,	    	1,	 	1 },	 
+	/* class      		instance    	title			tags mask		isfloating		monitor 	scratch key */
+	{ "Gimp",     		NULL,   	NULL,       		0,     	       	1,           	-1,			0 },
+	{ "firefox",  		NULL,   	NULL,       		1 << 4,       	0,           	-1,			0 },
+	{ "discord",  		NULL,   	NULL,       		1 << 2,       	0,           	2,			0 },
+	{ "lunarclient",	NULL,   	NULL,   			1 << 3,    		0,           	0,			0 },
+	{ "MultiMC5",  		NULL,   	NULL,       		1 << 3,      	0,           	0,			0 },
+	{ "Steam",  		NULL,		NULL,       		1 << 3,         0,           	-1,			0 },
+	{ "KeePassXC",  	NULL,   	NULL,       		1 << 6,     	0,           	-1,			0 },
+	{ "Signal",  		NULL,  	 	NULL,       		1 << 7,        	0,           	1,			0 },
+	{ "FreeTube",  		NULL,  	 	NULL,       		1 << 9,        	0,           	1,			0 },
+	{ "Yad",  		    NULL,  	 	NULL,       		0,        	    1,           	0,			0 },
+	{ NULL,	      		NULL,		"Picture in picture", 	0,	    	1,	 			1,			0 },
+	{ NULL,       		NULL,   	"scratchpad",   	0,            	1,           	-1,       	's' },
 };
 
 
@@ -95,10 +96,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "10", "-g", "5", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+/* First arg only serves to match against key in rules */
+static const char *scratchpadcmd[] = {"s", "alacritty", "-t", "scratchpad", NULL};
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
